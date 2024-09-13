41b5ff6bfa5a74b2d550f7b383bb6a94efcc8dfa
Rodrigo Tapia <95432602+rod641916@users.noreply.github.com>
2024-09-13 11:49:43 -0600

    Ejercicio 2 con función del curso

--- Ejercicio2/Ejercicio2.js
+++ Ejercicio2/Ejercicio2.js
@@ -1,3 +1,4 @@
+//////////////////Mi funcion////////////////////
 function polindromo(texto){
     let textoNormal= texto.toLowerCase();
     let textoInverso= textoNormal.split("").reverse().join("");
@@ -11,3 +12,16 @@ function polindromo(texto){
 
 console.log(polindromo("Otto"))
 
+
+console.log("**********Funcion del curso****************")
+////////////////////Funcion del curso//////////////////
+function palindromo1(texto){
+    let invertido = texto.split(' ').reverse().join('');
+    if(invertido===texto){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log("Es un palindromo? - " + palindromo1("Otto"));
