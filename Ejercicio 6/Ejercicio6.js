478efd4836dab24a01599960fd8de981e30b05d3
Rodrigo Tapia <95432602+rod641916@users.noreply.github.com>
2024-09-17 16:51:29 -0600

    Codigo de curso de ejercicio 6

--- Ejercicio 6/Ejercicio6.js
+++ Ejercicio 6/Ejercicio6.js
@@ -1,4 +1,6 @@
-function cuadrado(cantidad) {
+/////////////Mi codigo///////////////
+
+/*function cuadrado(cantidad) {
     for (let i = 0; i < cantidad; i++) {
         let linea = ""; // Reiniciamos la línea para cada fila
 
@@ -15,4 +17,33 @@ function cuadrado(cantidad) {
     }
 }
 
-cuadrado(10);
+cuadrado(5);*/
+
+
+//////////Codigo curso////////////////
+function lado(numero) {   //////////esta función pinta la linea de arriba y de abajo
+    let lado = "";
+
+    for (let i = 0; i < numero; i++) {
+        lado += "*";
+    }
+    return lado;
+}
+
+function cuadrado1(numero) {
+    let dibujo = lado(numero) + "\n";//// Se almacena el lado de arriba haciendo un salto de linea
+    let contenido = "";
+    for (let i = 0; i < numero - 2; i++) {// se restan los asteriscos que se mostraran arriva y abajo 
+        contenido = "*";
+
+        for (let x = 0; x < numero - 2; x++) {//// función para eliminar los asterisco de enmedio
+            contenido += " "
+        }
+        contenido += "*";
+        dibujo += contenido + "\n" ///se concatena al dibujo los lados con el salto de linea
+    }
+    dibujo += lado(numero);///se concatena a dibujo la linea de abajo
+    return dibujo;
+}
+
+console.log(cuadrado1(10))
