93637b0586fa7b2d5f5054412e8941dd1402e84a
Rodrigo Tapia <tapiareyesrodrigo@gmail.com>
2024-09-18 16:25:31 -0600

    Ejercico 8 completo

---
+++ Ejercicio 8/Ejercicio8.js
@@ -0,0 +1,18 @@
+////Mi codigo////
+
+function invertirnum(num){
+    let numeroinvertido=num.toString().split('').reverse().join("");
+    return parseInt(numeroinvertido);
+}
+
+console.log(invertirnum(56))
+
+
+////Codigo curso////
+
+function invertirNumero(numero){
+    let invertido = parseInt(numero.toString().split('').reverse().join('')) * Math.sign(numero);// La función sign detecta que signo se está pasando
+    return invertido
+}
+
+console.log(invertirNumero (78));
